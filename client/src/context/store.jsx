d4774d43416665160a7d41922bf98b10cd42056f
Clivon Osire <clivon254@icloud.com>
2024-09-27 20:57:25 +0300

    like

--- client/src/context/store.jsx
+++ client/src/context/store.jsx
@@ -36,7 +36,7 @@ export default function StoreContextProvider(props) {
 
           setError(false)
 
-          const res = await axios.post(url + '/api/post/stats')
+          const res = await axios.post('/api/post/stats')
 
           if(res.data.success)
           {
@@ -67,7 +67,7 @@ export default function StoreContextProvider(props) {
 
         setError(false)
 
-        const res = await axios.get(url + "/api/post/get-posts")
+        const res = await axios.get("/api/post/get-posts")
 
         if(res.data.success)
         {
