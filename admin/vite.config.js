3430f89f5d56724b9748742bf0ef6689ab7b5009
Clivon Osire <clivon254@icloud.com>
2024-09-27 18:19:43 +0300

    config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://coop-server.onrender.com/',
-        secure:true
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://coop-server.onrender.com/',
+        changeOrigin: true, // <--- Add this line
+        secure: true,
       },
     },
   },
