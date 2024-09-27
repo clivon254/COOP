0d18b582d4e610a928928abd6dbb0e3933607962
Clivon Osire <clivon254@icloud.com>
2024-09-27 17:50:54 +0300

    url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:800',
+        target:'https://coop-server.onrender.com/',
         secure:false
       },
     },
